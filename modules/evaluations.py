f10550f24202eb1dcf4d199b09cb80a11d7401b7
charlelucky <xingbod@gmail.com>
2020-05-14 15:00:32 +0800

    updated test

--- modules/evaluations.py
+++ modules/evaluations.py
@@ -24,16 +24,6 @@ def get_val_pair(path, name):
     return carray, issame
 
 
-def get_val_data(data_path):
-    """get validation data"""
-    lfw, lfw_issame = get_val_pair(data_path, 'lfw_align_112/lfw')
-    agedb_30, agedb_30_issame = get_val_pair(data_path,
-                                             'agedb_align_112/AgeDB/agedb_30')
-    cfp_fp, cfp_fp_issame = get_val_pair(data_path, 'cfp_align_112/cfp_fp')
-
-    return lfw, agedb_30, cfp_fp, lfw_issame, agedb_30_issame, cfp_fp_issame
-
-
 def ccrop_batch(imgs):
     assert len(imgs.shape) == 4
     resized_imgs = np.array([cv2.resize(img, (128, 128)) for img in imgs])
@@ -109,100 +99,6 @@ def calculate_roc(thresholds, embeddings1, embeddings2, actual_issame,
     # print('Equal Error Rate (EER): %1.3f' % eer)
 
     return tpr, fpr, accuracy, best_thresholds,auc,eer
-
-
-def calculate_roc_cosine(thresholds, embeddings1, embeddings2, actual_issame,
-                  nrof_folds=10,cfg=None):
-    assert (embeddings1.shape[0] == embeddings2.shape[0])
-    assert (embeddings1.shape[1] == embeddings2.shape[1])
-    nrof_pairs = min(len(actual_issame), embeddings1.shape[0])
-    nrof_thresholds = len(thresholds)
-    k_fold = KFold(n_splits=nrof_folds, shuffle=False)
-
-    tprs = np.zeros((nrof_folds, nrof_thresholds))
-    fprs = np.zeros((nrof_folds, nrof_thresholds))
-    accuracy = np.zeros((nrof_folds))
-    best_thresholds = np.zeros((nrof_folds))
-    indices = np.arange(nrof_pairs)
-
-    embeddings1_norm = tf.reduce_sum(embeddings1, axis=1)
-    embeddings2_norm = tf.reduce_sum(embeddings2, axis=1)
-    factor = embeddings1_norm + embeddings2_norm
-
-    diff = tf.reduce_sum(tf.multiply(embeddings1, embeddings2), axis=1)
-
-    dist = tf.divide(diff, factor)
-    # a_n_vec = tf.divide(a_n_product, denom2)
-
-    # diff = np.subtract(embeddings1, embeddings2)
-    # dist = np.sum(np.square(diff), 1)
-    # if cfg['head_type'] == 'IoMHead':
-    #     # dist = dist/(cfg['q']*cfg['embd_shape']) # should divide by the largest distance
-    #     dist = dist / (cfg['q']*100 )  # should divide by the largest distance
-    # print(dist)
-    for fold_idx, (train_set, test_set) in enumerate(k_fold.split(indices)):
-        # Find the best threshold for the fold
-        acc_train = np.zeros((nrof_thresholds))
-        for threshold_idx, threshold in enumerate(thresholds):
-            _, _, acc_train[threshold_idx] = calculate_accuracy(
-                threshold, dist[train_set], actual_issame[train_set])
-        best_threshold_index = np.argmax(acc_train)
-
-        best_thresholds[fold_idx] = thresholds[best_threshold_index]
-        for threshold_idx, threshold in enumerate(thresholds):
-            tprs[fold_idx, threshold_idx], fprs[fold_idx, threshold_idx], _ = \
-                calculate_accuracy(threshold,
-                                   dist[test_set],
-                                   actual_issame[test_set])
-        _, _, accuracy[fold_idx] = calculate_accuracy(
-            thresholds[best_threshold_index],
-            dist[test_set],
-            actual_issame[test_set])
-
-    tpr = np.mean(tprs, 0)
-    fpr = np.mean(fprs, 0)
-    return tpr, fpr, accuracy, best_thresholds
-
-def evaluate(embeddings, actual_issame, nrof_folds=10,cfg=None):
-    # Calculate evaluation metrics
-    thresholds = np.arange(0, 4, 0.01)
-    embeddings1 = embeddings[0::2]# 隔行采样
-    embeddings2 = embeddings[1::2]# 隔行采样
-    tpr, fpr, accuracy, best_thresholds,auc,eer= calculate_roc(
-        thresholds, embeddings1, embeddings2, np.asarray(actual_issame),
-        nrof_folds=nrof_folds, cfg=cfg)
-
-    return tpr, fpr, accuracy, best_thresholds,auc,eer
-
-
-def perform_val(embedding_size, batch_size, model,
-                carray, issame, nrof_folds=10, is_ccrop=False, is_flip=False,cfg=None):
-    """perform val"""
-
-    embeddings = np.zeros([len(carray), embedding_size])
-
-    for idx in tqdm.tqdm(range(0, len(carray), batch_size)):
-        batch = carray[idx:idx + batch_size]
-        batch = np.transpose(batch, [0, 2, 3, 1]) * 0.5 + 0.5
-        if is_ccrop:
-            batch = ccrop_batch(batch)
-
-        if is_flip:
-            fliped = hflip_batch(batch)
-            emb_batch = model(batch) + model(fliped)
-            # embeddings[idx:idx + batch_size] = l2_norm(emb_batch)
-        else:
-            batch = ccrop_batch(batch)
-            emb_batch = model(batch)
-        # print(emb_batch)
-
-        embeddings[idx:idx + batch_size] = l2_norm(emb_batch)
-        # embeddings[idx:idx + batch_size] = l2_norm(emb_batch)
-        # print(embeddings)
-    tpr, fpr, accuracy, best_thresholds,auc,eer = evaluate(
-        embeddings, issame, nrof_folds,cfg)
-
-    return accuracy.mean(), best_thresholds.mean(),auc,eer,embeddings
 
 
 
