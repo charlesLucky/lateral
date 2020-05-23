ef5caceaf78441c16dbb28f06fe6065c9602aa4b
charlelucky <xingbod@gmail.com>
2020-05-23 14:32:15 +0800

    updated test

--- train_debug.py
+++ train_debug.py
@@ -64,7 +64,7 @@ def main(_):
         SPLIT_WEIGHTS = (0.9, 0.1, 0.0)  # train cv val vs test
         myloadData = LoadFishDataUtil( './data/tmp_tent/SESSION1_ST_AUGMENT', 32, cfg['input_size_w'], cfg['input_size_h'], CLASS_NAMES, SPLIT_WEIGHTS)
         train_dataset, val_dataset, test_dataset, STEPS_PER_EPOCH, CLASS_NAMES, class_num = myloadData.loadFishData()
-        print(f'total class:{class_num},batch size '+cfg['batch_size'])
+        print(f'total class:{class_num}')
 
     else:
         logging.info("load fake dataset.")
