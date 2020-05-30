074322965ff0a79acf89a1d6c1c6838cb1708d04
charlelucky <xingbod@gmail.com>
2020-05-31 00:54:57 +0800

    updated test

--- modules/MDCM.py
+++ modules/MDCM.py
@@ -115,6 +115,7 @@ def getMDCM(input_shape=None, name="Multi-scale-Dilated"):
                kernel_regularizer=regularizers.l2(weight_decay))(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.5)(x)
 
     x = Conv2D(256, kernal_size, padding='same', activation='relu',
                kernel_regularizer=regularizers.l2(weight_decay))(x)
@@ -125,6 +126,7 @@ def getMDCM(input_shape=None, name="Multi-scale-Dilated"):
                kernel_regularizer=regularizers.l2(weight_decay))(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.5)(x)
 
     x = Conv2D(512, kernal_size, padding='same', activation='relu',
                kernel_regularizer=regularizers.l2(weight_decay))(x)
