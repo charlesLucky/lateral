c1511e239d4f68c68eec920260bf18b1e5ee11e5
charleslucky <xingbod@gmail.com>
2020-11-29 19:54:10 +0800

    2020-10-28 re-confirm

--- modules/dataset.py
+++ modules/dataset.py
@@ -354,7 +354,7 @@ def loadTrainDS(test_data_dir, BATCH_SIZE=64, cfg=None):
 
     def _transform_images(is_ccrop=False, cfg=None):
         def transform_images(x_train):
-            x_train = tf.image.resize(x_train, (160, 160))
+            x_train = tf.image.resize(x_train, (cfg['input_size_w']+50, cfg['input_size_h']+50))
             x_train = tf.image.random_crop(x_train, (cfg['input_size_w'], cfg['input_size_h'], 3))
             x_train = tf.image.random_flip_left_right(x_train)
             x_train = tf.image.random_saturation(x_train, 0.6, 0.8)
